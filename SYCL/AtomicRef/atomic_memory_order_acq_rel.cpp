3457ecf0b0b9f2bf442c03b66739e007ed318502
Lamzed-Short, Andrew <andrew.lamzed-short@intel.com>
2023-03-21 04:20:48 -0700

    Re-added cuda-specific test details

--- SYCL/AtomicRef/atomic_memory_order_acq_rel.cpp
+++ SYCL/AtomicRef/atomic_memory_order_acq_rel.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -O3 -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -O3 -o %t.out -Xsycl-target-backend=nvptx64-nvidia-cuda --cuda-gpu-arch=sm_70
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
